d530e104608c1890eb4ddbf099152fbf5c02fb32
Rose <rosem1696@gmail.com>
2018-02-08 12:26:50 -0600

    Added template source and header files

---
+++ template.c
@@ -0,0 +1,17 @@
+/*** Standard Library ***/
+
+/*** External Libraries ***/
+
+/*** Local Headers ***/
+
+/*** Local Defines ***/
+
+/*** Local Enums ***/
+
+/*** Local Structs ***/
+
+/*** Local Functions ***/
+
+/*** Local Variables ***/
+
+/*** Function Implementations ***/
---
+++ template.h
@@ -0,0 +1,20 @@
+#ifndef header_h
+#define header_h
+
+/*** Standard Library ***/
+
+/*** External Libraries ***/
+
+/*** Local Headers ***/
+
+/*** Global Defines ***/
+
+/*** Global Enums ***/
+
+/*** Global Structs ***/
+
+/*** Extern Variables ***/
+
+/*** Global Functions ***/
+
+#endif //header_h
